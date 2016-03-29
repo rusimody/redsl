e698cf163f2fa014feb5a7fd0a0ac42cef256729
Rusi Mody <rustompmody@gmail.com>
2016-03-29 17:24:12 +0530

    repost contents permuted about a bit

--- repost.py
+++ repost.py
@@ -1,27 +1,29 @@
-from collections import defaultdict
 """dsl input for redsl
-The names
-'rexp' for regular exp
-and 'postprocessors' for dict of postprocessors
+Should have available for importing these two names
+- 'rexp' for regular exp
+- 'postprocessors' for dict of postprocessors
 need to be exactly those names; rest (eg seriesmap) is just helpers for 
 those two
 """
 
-seriesmap = defaultdict(lambda : "No series type",
-                        {'.':"Plain", '-':"Preferred", '+':"Warrant"})
-postprocessors = {'issued' : bool, 'sertype': seriesmap, 'rights':bool}
-
-rexp = r"""
-# DSL (instantiation) for describing NYSE symbology
+# RE for describing NYSE symbology
 # From https://www.nyse.com/publicdocs/nyse/markets/amex-options/ArcaDirectAPISpecVersion4_1.pdf
 # Symbology table pg 12
+
+rexp = r"""
 
 (?=^.+$)				# There better be something >=1
 ^					# From beginning
 (?P<scrip>		[A-Z]*)		# The base scrip
 (?P<sertype>		[.+-])?		# Series type char
 (?P<series>		[A-QS-Z])?	# Series
-(?P<rights>		[R])?		# Series
+(?P<rights>		[R])?		# Rights (special case)
 (?P<issued>		[#])?		# issued char indicator
 $					# Thats all (there should be!)
 """
+
+# Postprocessors for above
+from collections import defaultdict
+seriesmap = defaultdict(lambda : "No series type",
+                        {'.':"Plain", '-':"Preferred", '+':"Warrant"})
+postprocessors = {'issued' : bool, 'sertype': seriesmap, 'rights':bool}
